b5204bbac589b38f285c1e6c65b73685622b0b54
1995cooperlee <92667451+1995cooperlee@users.noreply.github.com>
2021-10-18 11:46:13 +0800

    Update GitTest.java
    为了gitee改的

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git4！");
         System.out.println("push git4！");
          System.out.println("pull git5,github！");
+         System.out.println("辟邪剑谱，果然厉害！！！");
         
     }
 }
