05390d52dc0aca2512f116a7ee061f0b2344afc3
1995cooperlee <92667451+1995cooperlee@users.noreply.github.com>
2021-10-18 11:15:16 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("hello git2！");
         System.out.println("hello git4！");
         System.out.println("push git4！");
+         System.out.println("pull git5,github！");
+        
     }
 }
